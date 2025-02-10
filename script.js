25bfa4c7c1e06e41637f3c6dc023bfd049c542fe
Marko Tintor <marko.tintor.18@singimail.rs>
2025-02-10 13:24:46 +0100

    added classes

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const billAmount = document.querySelector("#bill-amount");
-const tipPercentage = document.querySelector("#tip-percentage");
-const calcButton = document.querySelector("#calculate-btn");
-const total = document.querySelector("#total-amount");
+class Calculator {
+  constructor() {
+    this.billAmount = document.querySelector("#bill-amount");
+    this.tipPercentage = document.querySelector("#tip-percentage");
+    this.calcButton = document.querySelector("#calculate-btn");
+    this.total = document.querySelector("#total-amount");
 
-calcButton.addEventListener("click", function () {
-  let enterBill = parseInt(billAmount.value);
-  let enterPercentage = parseInt(tipPercentage.value);
-  let percentage = (enterBill * enterPercentage) / 100;
-
-  if (
-    isNaN(enterBill) ||
-    isNaN(enterPercentage) ||
-    enterBill <= 0 ||
-    enterPercentage < 0
-  ) {
-    total.textContent = "Please enter valid value";
-    return;
+    this.addEventListeners();
   }
 
-  total.textContent = (percentage + enterBill).toFixed(2);
-});
+  addEventListeners() {
+    this.calcButton.addEventListener("click", () => this.calculateTotal());
+  }
+
+  calculateTotal() {
+    let enterBill = parseInt(this.billAmount.value);
+    let enterPercentage = parseInt(this.tipPercentage.value);
+    let percentage = (enterBill * enterPercentage) / 100;
+
+    if (
+      isNaN(enterBill) ||
+      isNaN(enterPercentage) ||
+      enterBill <= 0 ||
+      enterPercentage < 0
+    ) {
+      this.total.textContent = "Please enter valid value";
+      return;
+    }
+
+    this.total.textContent = (percentage + enterBill).toFixed(2);
+  }
+}
+
+const tipCalculator = new Calculator();
